d0112a1d48d2a76b801b2e7c2f02360896e5472f
dieego17 <diegorubio1704.drs@gmail.com>
2023-10-13 11:52:28 +0200

    complete

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const header_icon = document.getElementById("header_icon");
+const header_items = document.getElementById("header_items");
+
+const header_search = document.getElementById("header_search");
+const header_form = document.getElementById("header_form");
+
+
+
+header_icon.addEventListener("click", () => {
+ 
+});
+
+header_search.addEventListener("click", () => {
+  
+});
