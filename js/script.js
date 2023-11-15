c99345291ad95c977d5d64f6a3349eafe0fbd90f
Diego Rubio <diegorubio1704.drs@gmail.com>
2023-11-15 12:39:38 +0100

    the last commit

--- js/script.js
+++ js/script.js
@@ -4,12 +4,41 @@ const header_items = document.getElementById("header_items");
 const header_search = document.getElementById("header_search");
 const header_form = document.getElementById("header_form");
 
-
+document.addEventListener("DOMContentLoaded", () => {
+    header_form.classList.add("displaynone")
+    header_items.classList.add("displaynone")
+});
 
 header_icon.addEventListener("click", () => {
  
 });
 
 header_search.addEventListener("click", () => {
-  
+  header_items.classList.remove("displayflex")
+  header_items.classList.add("displaynone")
+  if(header_form.classList.contains("displaynone")){
+    header_form.classList.remove("displaynone")
+    header_form.classList.add("displayflex")
+  }else{
+    header_form.classList.remove("displayflex")
+    header_form.classList.add("displaynone")
+  }
+
 });
+
+header_icon.addEventListener("click", () => {
+  header_form.classList.remove("displayflex")
+  header_form.classList.add("displaynone")
+  if(header_items.classList.contains("displaynone")){
+    header_items.classList.remove("displaynone")
+    header_items.classList.add("displayflex")
+  }else{
+    header_items.classList.remove("displayflex")
+    header_items.classList.add("displaynone")
+  }
+
+});
+
+
+
+
